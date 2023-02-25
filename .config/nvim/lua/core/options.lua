5bbbeb93260b4c43da6cbfdd71ca9c99eb9b7b8b
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2023-02-24 17:28:27 -0800

    feat: get file types working

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -3,4 +3,13 @@ local settings = require("core.settings")
 local o = vim.opt
 local fn = vim.fn
 
+
+o.backup = false -- disable backups
+o.writebackup = false -- disable backups
+o.clipboard = 'unnamedplus' -- copy from vim
+o.completeopt = "menu,menuone,noselect" -- A comma separated list of options for Insert mode completion
+-- Line Length, Numbering
+
+o.expandtab = true -- use spaces instead of tabs
 o.termguicolors = true -- set term gui true colors (most terminals support this)
+
