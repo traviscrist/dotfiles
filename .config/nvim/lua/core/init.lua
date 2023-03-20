430f653f5484ce54c3af89164a1bdcae6a1d6ab4
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2023-03-20 16:58:01 -0700

    fix: get things to load in the right order

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -4,9 +4,9 @@
 require('core.setup')
 -- Settings
 require('core.options')
+-- Plugin management via lazy
+require('core.lazy')
 -- Key Mappings
 require('core.mappings')
 -- Autocmd
 require('core.autocmd')
--- Plugin management via lazy
-require('core.lazy')
