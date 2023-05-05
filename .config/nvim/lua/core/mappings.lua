78b8eb24f7bff96f458a3c618857be09144e6b48
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2023-05-05 11:12:41 -0700

    feat: add fzf lsp bindings

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -6,6 +6,7 @@ map('n', '<C-K>', '<C-W><C-K>')
 map('n', '<C-L>', '<C-W><C-L>')
 map('n', '<C-H>', '<C-W><C-H>')
 
+-- File Writing
 map('n', '<leader>w', '<cmd>write<cr>')
 map('n', '<leader>q', '<cmd>wq<cr>')
 map('n', '<leader>qq', '<cmd>quit<cr>')
@@ -15,7 +16,7 @@ map('n', '<leader>b', ':FzfLua buffers<cr>')
 map('n', '<leader>m', ':FzfLua oldfiles<cr>')
 map('n', '<leader>e', ':FzfLua git_files<cr>')
 map('n', '<leader>g', ':FzfLua grep_project<cr>')
-map('n', '<leader>v', ':FzfLua grep_cword<cr>')
+map('n', '<leader>f', ':FzfLua grep_cword<cr>')
 
 -- NeoTree Settings
 map('n', '<leader>t', ':NeoTreeFocusToggle<CR>')
