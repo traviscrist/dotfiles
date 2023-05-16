ba1fcb5a9e334ca792eb0d975c486f26a6c0c414
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2023-05-15 17:41:11 -0700

    feat: add barbecue and focus

---
+++ .config/nvim/lua/core/plugins/barbecue.lua
@@ -0,0 +1,14 @@
+local M = {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  config = function()
+    require('barbecue').setup({})
+  end
+}
+
+return M
---
+++ .config/nvim/lua/core/plugins/focus.lua
@@ -0,0 +1,8 @@
+local M = {
+  'beauwilliams/focus.nvim',
+  config = function()
+    require('focus').setup({})
+  end
+}
+
+return M
