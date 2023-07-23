541a2f0c7ad7ba57d40f166f82d16e722056db8c
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2023-07-22 21:26:23 -0700

    fix: reduce copilot priority

--- .config/nvim/lua/core/plugins/cmp.lua
+++ .config/nvim/lua/core/plugins/cmp.lua
@@ -19,9 +19,7 @@ local M = {
       if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-    end
-
-    cmp.setup({
+    end cmp.setup({
       formatting = {
         format = lspkind.cmp_format({
           maxwidth = 50,
@@ -66,10 +64,10 @@ local M = {
       },
       sources = {
         -- Other Sources
-        { name = "nvim_lsp" },
-        { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lsp", priority = 2 },
+        { name = "nvim_lsp_signature_help", priority = 2 },
         -- Copilot Source
-        { name = "copilot" },
+        { name = "copilot", priority = 3 },
         { name = "buffer",                 keyword_length = 5 },
         { name = "snippy" },
         { name = "path" },
@@ -78,12 +76,11 @@ local M = {
       sorting = {
         priority_weight = 2,
         comparators = {
+          cmp.config.compare.exact,
           require("copilot_cmp.comparators").prioritize,
-
           -- Below is the default comparitor list and order for nvim-cmp
           cmp.config.compare.offset,
           -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-          cmp.config.compare.exact,
           cmp.config.compare.score,
           cmp.config.compare.recently_used,
           cmp.config.compare.locality,
