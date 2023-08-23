7a43878bd48f147b8dbe140f3293003cfd8836bd
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2023-08-22 19:58:43 -0700

    fix: disable auto format on kotlin for now

--- .config/nvim/lua/core/plugins/lsp-format.lua
+++ .config/nvim/lua/core/plugins/lsp-format.lua
@@ -1,7 +1,11 @@
 local M = {
   'lukas-reineke/lsp-format.nvim',
   config = function()
-    require('lsp-format').setup({})
+    require('lsp-format').setup({
+      kt = {
+        exclude = { 'kotlin_language_server' }
+      }
+    })
   end
 }
 
