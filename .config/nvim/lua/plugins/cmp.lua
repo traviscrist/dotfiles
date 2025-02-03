2addfb2e74e7dbe2d3640c99723a64063892528b
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2025-02-03 12:16:59 -0800

    fix: completion not working on enter for snippets

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -9,6 +9,8 @@ local M = {
     { "iguanacucumber/mag-buffer",   name = "cmp-buffer" },
     { "iguanacucumber/mag-cmdline",  name = "cmp-cmdline" },
     "hrsh7th/cmp-nvim-lsp-signature-help",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip"
   },
   config = function()
     local cmp = require("cmp")
@@ -27,7 +29,7 @@ local M = {
       },
       snippet = {
         expand = function(args)
-          -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         end,
       },
       window = {
@@ -39,7 +41,7 @@ local M = {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({  select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() and has_words_before() then
             cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
