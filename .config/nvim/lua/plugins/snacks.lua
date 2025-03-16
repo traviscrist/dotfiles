0cfb30ca4aa723fe5abac838ac300b0c70d649c5
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2025-03-15 23:36:25 -0400

    feat: ivy split as options

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -25,7 +25,23 @@ local M = {
     },
     picker = {
       layout = {
-        preset = "ivy_split"
+        preview = "main",
+        layout = {
+          box = "vertical",
+          backdrop = false,
+          width = 0,
+          height = 0.4,
+          position = "bottom",
+          border = "top",
+          title = " {title} {live} {flags}",
+          title_pos = "left",
+          { win = "input", height = 1, border = "bottom" },
+          {
+            box = "horizontal",
+            { win = "list",    border = "none" },
+            { win = "preview", title = "{preview}", width = 0.6, border = "left" },
+          },
+        },
       },
       sources = {
         explorer = {
