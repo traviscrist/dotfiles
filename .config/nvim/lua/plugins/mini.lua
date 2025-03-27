bcc8d4f7371b588c5d7724367bdeb5148be84a83
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2025-03-27 14:03:41 -0700

    fix: mini plugins not working

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
   'echasnovski/mini.nvim',
   version = '*',
   config = function()
-    require('mini.surround').setup({})
-    require('mini.pairs').setup({})
+    require('mini.surround').setup()
+    require('mini.pairs').setup()
   end
 }
