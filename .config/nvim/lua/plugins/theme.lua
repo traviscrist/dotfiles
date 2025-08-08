e528491ac2501526b254ff3279a7dba51c990db5
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2025-08-08 15:13:41 -0700

    feat: theme updates

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -8,13 +8,13 @@ return {
     require("everforest").setup({
       -- Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
       -- Default is "medium".
-      background = "medium",
+      background = "soft",
       -- How much of the background should be transparent. Options are 0, 1 or 2.
       -- Default is 0.
       --
       -- 2 will have more UI components be transparent (e.g. status line
       -- background).
-      transparent_background_level = 0,
+      transparent_background_level = 2,
       -- Whether italics should be used for keywords, builtin types and more.
       italics = false,
       -- Disable italic fonts for comments. Comments are in italics by default, set
