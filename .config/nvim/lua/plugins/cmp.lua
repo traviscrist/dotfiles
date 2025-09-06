62b8c3e3a5549f131617dcc85f24c22e7e7ef1ae
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2025-09-06 13:38:40 -0700

    fix: diable copolit

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -70,7 +70,7 @@ local M = {
       sorting = {
         priority_weight = 2,
         comparators = {
-          require("copilot_cmp.comparators").prioritize,
+          -- require("copilot_cmp.comparators").prioritize,
           -- Below is the default comparitor list and order for nvim-cmp
           cmp.config.compare.offset,
           -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
